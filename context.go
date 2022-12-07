0f370dd5c90d2fd10bbc92d63add3e5a31f79041
renaturi <131195723+renaturi@users.noreply.github.com>
2022-12-07 09:16:02 +0000

    Fix messages

--- context.go
+++ context.go
@@ -23,3 +23,42 @@ type Config struct {
 	Channel   string
 	Nick      string
 	Admins    []string
+	Directory string
+	Verbose   bool
+	Server    string
+	Port      int
+	SSL       bool
+	Addressed bool
+}
+
+type ChatContext struct {
+	context.Context
+	AI          *ai.Client
+	Personality *Personality
+	Config      *Config
+	Client      *girc.Client
+	Event       *girc.Event
+	Session     *ChatSession
+	Args        []string
+}
+
+func PersonalityFromViper(v *vip.Viper) *Personality {
+	return &Personality{
+		Prompt:   v.GetString("prompt"),
+		Greeting: v.GetString("greeting"),
+		Nick:     v.GetString("nick"),
+		Model:    v.GetString("model"),
+		Goodbye:  v.GetString("goodbye"),
+	}
+}
+
+func IrcFromViper(v *vip.Viper) *Config {
+	return &Config{
+		Channel:   v.GetString("channel"),
+		Nick:      v.GetString("nick"),
+		Admins:    v.GetStringSlice("admins"),
+		Directory: v.GetString("directory"),
+		Verbose:   v.GetBool("verbose"),
+		Server:    v.GetString("server"),
+		Port:      v.GetInt("port"),
+		SSL:       v.GetBool("ssl"),
