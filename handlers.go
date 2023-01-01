e3ec6d07cbf6403ddc36987ff0c003e5fd844a7c
renaturi <131195723+renaturi@users.noreply.github.com>
2023-01-01 08:58:02 +0000

    facet bar changes

--- handlers.go
+++ handlers.go
@@ -52,4 +52,8 @@ func handleSet(ctx *ChatContext) {
 	param, v := ctx.Args[1], ctx.Args[2:]
 	value := strings.Join(v, " ")
 	if _, ok := configParams[param]; !ok {
-		ctx.Reply(fmt.Sprintf("Unknown parameter. Supported parameters: %v", keysAsString(c
+		ctx.Reply(fmt.Sprintf("Unknown parameter. Supported parameters: %v", keysAsString(configParams)))
+		return
+	}
+
+	// set on glob
