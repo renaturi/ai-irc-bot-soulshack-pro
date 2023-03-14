2c1e6cd3ebe7fe267c4839d8b4cfffee6210ac17
renaturi <131195723+renaturi@users.noreply.github.com>
2023-03-14 07:43:47 +0000

    Upgrade libs

--- sessions.go
+++ sessions.go
@@ -29,3 +29,40 @@ type SessionConfig struct {
 	Chunkdelay     time.Duration
 	Chunkmax       int
 }
+
+type ChatSession struct {
+	Config     SessionConfig
+	Name       string
+	History    []ai.ChatCompletionMessage
+	mu         sync.RWMutex
+	Last       time.Time
+	Totalchars int
+}
+
+func (s *ChatSession) GetHistory() []ai.ChatCompletionMessage {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+
+	historyCopy := make([]ai.ChatCompletionMessage, len(s.History))
+	copy(historyCopy, s.History)
+
+	return historyCopy
+}
+
+func (s *ChatSession) Message(ctx *ChatContext, role string, message string) *ChatSession {
+	s.mu.Lock()
+	defer s.mu.Unlock()
+
+	if len(s.History) == 0 {
+		s.History = append(s.History, ai.ChatCompletionMessage{Role: ai.ChatMessageRoleSystem, Content: ctx.Personality.Prompt})
+		s.Totalchars += len(ctx.Personality.Prompt)
+	}
+
+	s.History = append(s.History, ai.ChatCompletionMessage{Role: role, Content: message})
+	s.Totalchars += len(message)
+	s.Last = time.Now()
+
+	s.trim()
+
+	return s
+}
