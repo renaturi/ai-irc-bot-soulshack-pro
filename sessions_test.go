901ff273eead64fc6ead97c48246fe96ad8f316d
renaturi <131195723+renaturi@users.noreply.github.com>
2023-03-15 14:05:24 +0000

    added proxy support

--- sessions_test.go
+++ sessions_test.go
@@ -9,3 +9,46 @@ import (
 	"sync"
 	"testing"
 	"time"
+
+	ai "github.com/sashabaranov/go-openai"
+	vip "github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestChatSession(t *testing.T) {
+	vip.Set("session", 1*time.Hour)
+	vip.Set("history", 10)
+
+	//log.SetOutput(io.Discard)
+
+	ctx := &ChatContext{
+		Personality: &Personality{
+			Prompt: "You are a helpful assistant.",
+		},
+	}
+
+	t.Run("Test interactions and message history", func(t *testing.T) {
+		session1 := sessions.Get("session1")
+		session1.Message(ctx, ai.ChatMessageRoleUser, "Hello!")
+		session1.Message(ctx, ai.ChatMessageRoleAssistant, "Hi there!")
+
+		assert.Len(t, session1.History, 3)
+		assert.Equal(t, session1.History[1].Content, "Hello!")
+		assert.Equal(t, session1.History[2].Content, "Hi there!")
+	})
+}
+func TestExpiry(t *testing.T) {
+	//log.SetOutput(io.Discard)
+	ctx := &ChatContext{
+		Personality: &Personality{
+			Prompt: "You are a helpful assistant.",
+		},
+	}
+	t.Run("Test session expiration and trimming", func(t *testing.T) {
+		vip.Set("session", 500*time.Millisecond)
+		vip.Set("history", 20)
+
+		session2 := sessions.Get("session2")
+		session2.Message(ctx, ai.ChatMessageRoleUser, "How are you?")
+		session2.Message(ctx, ai.ChatMessageRoleAssistant, "I'm doing great, thanks!")
+		session2.Message(ctx, ai.ChatMessageRoleUser, "What's your name?")
