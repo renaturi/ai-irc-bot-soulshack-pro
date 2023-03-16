12c8d20f615803a3e32c7d57e98975d7a1069eb1
renaturi <131195723+renaturi@users.noreply.github.com>
2023-03-16 13:04:55 +0000

    specs passing

--- sessions_test.go
+++ sessions_test.go
@@ -52,3 +52,44 @@ func TestExpiry(t *testing.T) {
 		session2.Message(ctx, ai.ChatMessageRoleUser, "How are you?")
 		session2.Message(ctx, ai.ChatMessageRoleAssistant, "I'm doing great, thanks!")
 		session2.Message(ctx, ai.ChatMessageRoleUser, "What's your name?")
+
+		time.Sleep(2 * time.Second)
+		session3 := sessions.Get("session2")
+
+		assert.NotEqual(t, session2, session3, "Expired session should not be reused")
+		assert.Len(t, session3.History, 0, "New session history should be empty")
+
+		session3.Message(ctx, ai.ChatMessageRoleUser, "Hello again!")
+		session3.Message(ctx, ai.ChatMessageRoleAssistant, "Hi! Nice to see you again!")
+
+		assert.Len(t, session3.History, 3, "History should include the latest 2 messages plus the initial system message")
+		assert.Equal(t, session3.History[1].Content, "Hello again!")
+		assert.Equal(t, session3.History[2].Content, "Hi! Nice to see you again!")
+	})
+}
+
+func TestSessionConcurrency(t *testing.T) {
+	vip.Set("session", 1*time.Hour)
+	vip.Set("history", 10)
+
+	log.SetOutput(io.Discard)
+
+	t.Run("Test session concurrency", func(t *testing.T) {
+		vip.Set("session", 1*time.Hour)
+		vip.Set("history", 500*2000)
+
+		ctx := &ChatContext{
+			Personality: &Personality{
+				Prompt: "You are a helpful assistant.",
+			},
+		}
+
+		const concurrentUsers = 1000
+		const messagesPerUser = 500
+
+		startTime := time.Now()
+
+		var wg sync.WaitGroup
+		wg.Add(concurrentUsers)
+
+		for i := 0; i < concurrentUsers; i++ {
